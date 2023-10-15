59426087a9db78e055448a0bd11b2d18993da8aa
OlenaKyz <kyz102030@gmail.com>
2023-10-15 21:08:33 +0300

    классы и поля

--- C++_pr/FileName.cpp
+++ C++_pr/FileName.cpp
@@ -1,7 +1,56 @@
 #include<iostream>
 using namespace std;
 
+class Product
+{
+	string name;
+	string details;
+	double price;
+	int quantity;
+};
+
+class Stock
+{
+	Product* products;
+};
+
+class Basket
+{
+	Product* myProducts;
+	double totalPrice;
+};
+
+class User
+{
+	string login;
+	string password;
+};
+
+class UserAccounting
+{
+	User* users;
+};
+
+class Order
+{
+	Basket myBasket;
+	User buyer;
+	string adress;
+	string date;
+	string status;
+};
+
+class OrderAccounting
+{
+	Order* orders;
+};
+
+class PaymentDelivery
+{
+
+};
+
 int main()
 {
-	cout <<"hello"<< endl;
+
 }
