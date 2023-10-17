2c3e6fb6b2bbb3387b496d805ed60aeeee7557bf
QweRezer <Nikitaqwerez@ukr.net>
2023-10-17 12:13:42 +0300

    proj

--- C++_pr/FileName.cpp
+++ C++_pr/FileName.cpp
@@ -1,9 +1,162 @@
-#include<iostream>
+#include <iostream>
+#include <vector>
+
 using namespace std;
+
+class Product
+{
+public:
+    string name;
+    string description;
+    double price;
+    int quantity;
+
+    Product(string name, string description, double price, int quantity) 
+    {
+        this->name = name;
+        this->description = description;
+        this->price = price;
+        this->quantity = quantity;
+    }
+};
+
+
+class Cart
+{
+public:
+    vector<Product*> products;
+
+    void addProduct(Product* product)
+    {
+        products.push_back(product);
+    }
+
+    double getTotalPrice()
+    {
+        double totalPrice = 0;
+        for (Product* product : products)
+        {
+            totalPrice += product->price;
+        }
+        return totalPrice;
+    }
+};
+
+
+class Order
+{
+public:
+    int orderNumber;
+    double orderDate;
+    string orderStatus;
+    vector<Product*> products;
+    string userId;
+
+    Order(int orderNumber, double orderDate, string orderStatu, string userId)
+    {
+        this->orderNumber = orderNumber;
+        this->orderDate = orderDate;
+        this->orderStatus = orderStatus;
+
+    }
+
+    void addProduct(Product* product)
+    {
+        products.push_back(product);
+    }
+
+    double getTotalPrice()
+    {
+        double totalPrice = 0;
+        for (Product* product : products)
+        {
+            totalPrice += product->price;
+        }
+        return totalPrice;
+    }
+
+
+    void setOrderStatus(string status)
+    {
+        orderStatus = status;
+    }
+    void setUserId(string userId)
+    {
+        this->userId = userId;
+    }
+    string getOrderStatus() const 
+    {
+        return orderStatus;
+    }
+
+    void Print()
+    {
+        cout << "\nNomer zakaza: " << orderNumber << endl;
+        cout << "Data zakaza: " << orderDate << endl;
+
+        for (Product* product : products)
+        {
+            cout << "\nName: " << product->name << endl;
+            cout << "Description: " << product->description << endl;
+            cout << "Price: $" << product->price << endl;
+        }
+    }
+};
+
+
+class User
+{
+public:
+    string username;
+    string password;
+    vector<Order*> orders;
+
+    User(string username, string password)
+    {
+        this->username = username;
+        this->password = password;
+    }
+    void addOrder(Order* order)
+    {
+        orders.push_back(order);
+    }
+
+    string getUsername() const
+    {
+        return username;
+    }
+};
+
 
 int main()
 {
-	cout << endl;
+    User* user = new User("admin", "password");
+    
+    Product* product1 = new Product("iPhone 15", "Apple iPhone 15", 999, 10);
+    Product* product2 = new Product("Samsung Galaxy S24", "Samsung Galaxy S24", 899, 15);
+    Product* product3 = new Product("MacBook", "Apple MacBook", 1499, 5);
 
-	cout << "Nikita" << endl;
+
+    Cart* cart = new Cart();
+    cart->addProduct(product1);
+    cart->addProduct(product2);
+
+    double totalPrice = cart->getTotalPrice();
+    cout << "Stoimost tovarov v korzine: " << totalPrice << endl;
+
+    
+    Order* order = new Order(1, double(15.11), "Completed", user->username);
+    order->addProduct(product1);
+    order->Print();
+    order->setOrderStatus("Completed");
+
+
+    cout << "\nPolzovatel: " << user->getUsername() << endl;
+    cout << "Status zakaza: " << order->getOrderStatus() << endl;
+
+
+    totalPrice = order->getTotalPrice();
+    cout << "Stoimost tovarov v zakaze: " << totalPrice << endl;
+
+    return 0;
 }
