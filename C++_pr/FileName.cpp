8d5d393c58c2b6150665456c12b8677a24d867f9
OlenaKyz <kyz102030@gmail.com>
2023-10-21 19:17:37 +0300

    1

--- C++_pr/FileName.cpp
+++ C++_pr/FileName.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 #include <vector>
 #include <fstream>
-#include <locale>
+
 
 using namespace std;
 
@@ -14,7 +14,8 @@ public:
     double price;
     int quantity;
 
-    Product(string name, string description, double price, int quantity) {
+    Product(string name, string description, double price, int quantity)
+    {
         this->name = name;
         this->description = description;
         this->price = price;
@@ -23,7 +24,6 @@ public:
 
 
 };
-
 
 class Cart
 {
@@ -97,14 +97,14 @@ public:
 
     void Print()
     {
-        cout << "\n????? ??????: " << orderNumber << endl;
-        cout << "???? ??????: " << orderDate << endl;
+        cout << "\nNomer zakaza: " << orderNumber << endl;
+        cout << "Data zakaza: " << orderDate << endl;
 
         for (Product* product : products)
         {
-            cout << "\n????????: " << product->name << endl;
-            cout << "????????: " << product->description << endl;
-            cout << "????: $" << product->price << endl;
+            cout << "\nName: " << product->name << endl;
+            cout << "Description: " << product->description << endl;
+            cout << "Price: $" << product->price << endl;
         }
     }
 };
@@ -136,7 +136,6 @@ public:
 
 int main()
 {
-    setlocale(LC_ALL, "RU");
 
     User* user = new User("admin", "password");
 
@@ -150,7 +149,7 @@ int main()
     cart->addProduct(product2);
 
     double totalPrice = cart->getTotalPrice();
-    cout << "????? ????????? ??????? ? ???????: " << totalPrice << endl;
+    cout << "Obschaya stoimost tovarov v korzine: " << totalPrice << endl;
 
 
     Order* order = new Order(1, double(15.11), "Process", user->username);
@@ -159,30 +158,31 @@ int main()
     order->setOrderStatus("Completed");
 
 
-    cout << "\n?????: " << user->getUsername() << endl;
-    cout << "?????? ??????: " << order->getOrderStatus() << endl;
+    cout << "\nLogin: " << user->getUsername() << endl;
+    cout << "Status zakaza: " << order->getOrderStatus() << endl;
 
 
     totalPrice = order->getTotalPrice();
-    cout << "????? ????????? ??????? ? ??????: " << totalPrice << endl;
+    cout << "Obschaya stoimost tovarov v zakaze: " << totalPrice << endl;
 
 
     ofstream outputFile("Order.txt");
-    if (!outputFile.is_open()) {
+    if (!outputFile.is_open())
+    {
         cout << "error" << endl;
         return 1;
     }
-    outputFile << "\n????? ??????: " << order->orderNumber << endl;
-    outputFile << "???? ??????: " << order->orderDate << endl;
-    outputFile << "????? ????????????: " << order->userId << endl;
-    outputFile << "?????? ??????: " << order->orderStatus << endl;
-    outputFile << "?????? ? ??????:" << endl;
+    outputFile << "\nNomer zakaza: " << order->orderNumber << endl;
+    outputFile << "Data zakaza: " << order->orderDate << endl;
+    outputFile << "Login polzovatelya: " << order->userId << endl;
+    outputFile << "Status zakaza: " << order->orderStatus << endl;
+    outputFile << "Tovari v zakaze:" << endl;
 
     for (Product* product : order->products)
     {
-        outputFile << "\n????????: " << product->name << endl;
-        outputFile << "????????: " << product->description << endl;
-        outputFile << "????: $" << product->price << endl;
+        outputFile << "\nName: " << product->name << endl;
+        outputFile << "Description: " << product->description << endl;
+        outputFile << "Price: $" << product->price << endl;
     }
     outputFile.close();
 
