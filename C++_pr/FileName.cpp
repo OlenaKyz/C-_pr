33ded973fedf993ec2b522eac766fc253c606f48
OlenaKyz <kyz102030@gmail.com>
2023-10-21 19:17:23 +0300

    1

--- C++_pr/FileName.cpp
+++ C++_pr/FileName.cpp
@@ -1,236 +1,190 @@
-#include<iostream>
+#include <iostream>
 #include <vector>
+#include <fstream>
+#include <locale>
+
 using namespace std;
 
-class MyProduct
-{
-protected:
-	string name;
-	double price;
-	int quantity;
-public:
-	MyProduct(string myName, double myPrice, int myQuantity) :
-		name{ myName }, price{ myPrice }, quantity{ myQuantity } {}
-	//Product() : Product(0, 0, 0, 0) {}
-	string GetName()
-	{
-		return name;
-	}
-	double GetPrice()
-	{
-		return price;
-	}
-	int GetQuantity()
-	{
-		return quantity;
-	}
-};
 
-class Product : public MyProduct
+class Product
 {
-	string name;
-	string details;
-	double price;
-	int quantity;
 public:
-	Product(string myName, string myDetails, double myPrice, int myQuantity) :
-		MyProduct( myName,  myPrice,  myQuantity), details{ myDetails } {}
-	//Product() : Product(0, 0, 0, 0) {}
-	string GetDetails()
-	{
-		return details;
-	}
+    string name;
+    string description;
+    double price;
+    int quantity;
 
-};
-
-class Stock
-{
-	vector <Product> products;
-public:
+    Product(string name, string description, double price, int quantity) {
+        this->name = name;
+        this->description = description;
+        this->price = price;
+        this->quantity = quantity;
+    }
 
 
 };
 
-class Basket
+
+class Cart
 {
-	vector <MyProduct> myProducts;
-	double totalPrice=0;
 public:
-	void PrintBasket()
-	{
-		for (int i = 0; i < myProducts.size(); i++)
-		{
-			cout << myProducts[i].GetName() << ",\tprice " << myProducts[i].GetPrice() * myProducts[i].GetQuantity() << " hrn.,\tquantity " << myProducts[i].GetQuantity() << endl;
-			cout << "total price: " << totalPrice << endl;
-		}
-	}
-	void AddToBasket(string myName, double myPrice, int myQuantity)
-	{
-		MyProduct myProduct(myName, myPrice, myQuantity);
-		myProducts.push_back(myProduct);
-		totalPrice += (myPrice * myQuantity);
-	}
+    vector<Product*> products;
 
-	void DeletFromBasket(string productName)
-	{
-		int temp = 0;
-		for (int i = 0; i < myProducts.size(); i++)
-		{
-			if (myProducts[i].GetName() == productName)
-			{
-				myProducts.erase(myProducts.begin() + i);
-				temp++;
-			}
-		}
-		if (temp == 0) cout << "no such product in busket." << endl;
-	}
+    void addProduct(Product* product)
+    {
+        products.push_back(product);
+    }
 
-	//double GetTotalPrice()
-	//{
-	//	return totalPrice;
-	//}
+    double getTotalPrice()
+    {
+        double totalPrice = 0;
+        for (Product* product : products)
+        {
+            totalPrice += product->price;
+        }
+        return totalPrice;
+    }
 };
 
-class User
-{
-	string login;
-	string password;
-public:
-	User(string myLogin, string myPassword) :
-		login{ myLogin }, password{ myPassword }{}
-	//User() : User(0, 0) {}
-
-	string GetLogin()
-	{
-		return login;
-	}
-
-	string GetPassword()
-	{
-		return password;
-	}
-	void PrintUser()
-	{
-		cout << "User: " << login << endl;
-	}
-};
-
-class UserAccounting
-{
-	vector <User> users;
-public:
-	//UserAccounting() : users{ NULL } {}
-
-	//UserAccounting(const UserAccounting& copy)
-	//{
-	//	size = copy.size;
-	//	users = new User[size];
-	//	for (int i = 0; i < size; i++)
-	//	{
-	//		users[i]=copy.users[i];
-	//	}
-	//}
-	bool SearchUser(string mylogin, string mypassword)
-	{
-		for (int i = 0; i < users.size(); i++)
-		{
-			if (users[i].GetLogin() == mylogin && users[i].GetPassword() == mypassword) 
-				return true;
-			else return false;
-		}
-	}
-};
 
 class Order
 {
-	Basket basket;
-	User buyer;
-	string adress;
-	string date;
-	string statusPayment;
-	string statusDelivery;
-	//Order(const Basket& myBasket, const User& myBuyer, string myAdress, string myDate, string myStatusPayment, string myStatusDelivery) :
-	//	basket{ myBasket }, buyer{ myBuyer }, adress{ myAdress }, date{ myDate },
-	//	statusPayment { myStatusPayment }, statusDelivery { myStatusDelivery } {}
-	//	adress{0}, date{0}, statusPayment{0}, statusDelivery{0} 
-	//{
-	//	Basket baskeå;
-	//	User buyer;
-	//}
 public:
-	void PrintOrder()
-	{
-		buyer.PrintUser();
-		basket.PrintBasket();
-		cout << "adress: " << adress << endl;
-		cout << "date of order: " << date << endl;
-		cout << "status payment: " << statusPayment << endl;
-		cout << "status delivery: " << statusDelivery << endl;
-	}
+    int orderNumber;
+    double orderDate;
+    string orderStatus;
+    vector<Product*> products;
+    string userId;
 
-	int StatusPayment()
-	{
-		cout << "1 - pay for your order now.\n";
-		cout << "2 - pay for the order upon receipt.\n";
-		cout << "3 - refuse payment and exit the order.\n";
-		char ch;
-		while (ch != 1 && ch != 2 && ch != 3) cin >> ch;
-		if (ch == '1')
-		{
+    Order(int orderNumber, double orderDate, string orderStatu, string userId)
+    {
+        this->orderNumber = orderNumber;
+        this->orderDate = orderDate;
+        this->orderStatus = orderStatus;
 
-		}
-			return ch;
-	}
+    }
 
-	void Shopping()
-	{
+    void addProduct(Product* product)
+    {
+        products.push_back(product);
+    }
 
-	}
+    double getTotalPrice()
+    {
+        double totalPrice = 0;
+        for (Product* product : products)
+        {
+            totalPrice += product->price;
+        }
+        return totalPrice;
+    }
+
+
+    void setOrderStatus(string status)
+    {
+        orderStatus = status;
+    }
+
+
+    void setUserId(string userId)
+    {
+        this->userId = userId;
+    }
+
+    string getOrderStatus() const {
+        return orderStatus;
+    }
+
+    void Print()
+    {
+        cout << "\n????? ??????: " << orderNumber << endl;
+        cout << "???? ??????: " << orderDate << endl;
+
+        for (Product* product : products)
+        {
+            cout << "\n????????: " << product->name << endl;
+            cout << "????????: " << product->description << endl;
+            cout << "????: $" << product->price << endl;
+        }
+    }
 };
 
-class OrderAccounting
+
+class User
 {
-	vector <Order> orders;
 public:
-	//OrderAccounting() : orders{NULL} {}
+    string username;
+    string password;
+    vector<Order*> orders;
 
-	//OrderAccounting(const OrderAccounting& copy)
-	//{
-	//	size = copy.size;
-	//	orders = new Order[size];
-	//	for (int i = 0; i < size; i++)
-	//	{
-	//		orders[i] = copy.orders[i];
-	//	}
-	//}
-	void PrintOrders()
-	{
-		for (int i = 0; i < orders.size(); i++)
-		{
-			cout << i + 1 << ". order:"<<endl;
-			orders[i].PrintOrder();
-			cout << endl;
-		}
-	}
+    User(string username, string password)
+    {
+        this->username = username;
+        this->password = password;
+    }
+    void addOrder(Order* order)
+    {
+        orders.push_back(order);
+    }
 
-
+    string getUsername() const
+    {
+        return username;
+    }
 };
 
-//class PaymentDelivery
-//{
-//	int StatusPayment()
-//	{
-//		cout << "1 - pay for your order now.\n";
-//		cout << "2 - pay for the order upon receipt.\n";
-//		cout << "3 - refuse payment and exit the order.\n";
-//		char ch;
-//		while (ch != 1 && ch != 2 && ch != 3) cin >> ch;
-//		if (ch == '1')
-//		return ch;
-//	}
-//};
 
 int main()
 {
+    setlocale(LC_ALL, "RU");
 
+    User* user = new User("admin", "password");
+
+    Product* product1 = new Product("iPhone 15", "Apple iPhone 15", 999, 10);
+    Product* product2 = new Product("Samsung Galaxy S24", "Samsung Galaxy S24", 899, 15);
+    Product* product3 = new Product("MacBook", "Apple MacBook", 1499, 5);
+
+
+    Cart* cart = new Cart();
+    cart->addProduct(product1);
+    cart->addProduct(product2);
+
+    double totalPrice = cart->getTotalPrice();
+    cout << "????? ????????? ??????? ? ???????: " << totalPrice << endl;
+
+
+    Order* order = new Order(1, double(15.11), "Process", user->username);
+    order->addProduct(product1);
+    order->Print();
+    order->setOrderStatus("Completed");
+
+
+    cout << "\n?????: " << user->getUsername() << endl;
+    cout << "?????? ??????: " << order->getOrderStatus() << endl;
+
+
+    totalPrice = order->getTotalPrice();
+    cout << "????? ????????? ??????? ? ??????: " << totalPrice << endl;
+
+
+    ofstream outputFile("Order.txt");
+    if (!outputFile.is_open()) {
+        cout << "error" << endl;
+        return 1;
+    }
+    outputFile << "\n????? ??????: " << order->orderNumber << endl;
+    outputFile << "???? ??????: " << order->orderDate << endl;
+    outputFile << "????? ????????????: " << order->userId << endl;
+    outputFile << "?????? ??????: " << order->orderStatus << endl;
+    outputFile << "?????? ? ??????:" << endl;
+
+    for (Product* product : order->products)
+    {
+        outputFile << "\n????????: " << product->name << endl;
+        outputFile << "????????: " << product->description << endl;
+        outputFile << "????: $" << product->price << endl;
+    }
+    outputFile.close();
+
+    return 0;
 }
